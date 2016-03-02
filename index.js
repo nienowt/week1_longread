57d735e8caae70bfcf4866bc1e0b683b10a8148c
Thom Nienow <elstad.nienow@gmail.com>
2016-03-01 21:43:33 -0800

    topage35

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var server = require(__dirname + '/server');
+var router = require(__dirname + '/router');
+var requestHandlers = require(__dirname + '/requestHandlers');
+
+var handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
+
+
+stopped at PAGE 35 - top
